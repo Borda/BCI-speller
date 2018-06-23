393a2be627660bd566b6e598a8085c51347ad2e5
jirka <jirka.borovec@seznam.cz>
2018-06-23 20:33:24 +0200

    add init testing

--- bci/data_utils.py
+++ bci/data_utils.py
@@ -1,13 +1,33 @@
 """
-Basic hadling data
+Basic handling data
 
 """
+
+import os
 
 import numpy as np
 from scipy import signal
 from mne import filter
 
 SAMPLE_FREQ = 250.
+
+
+def update_path(path, max_up=5):
+    """ update the path by bobble up
+
+    :param str path:
+    :param int max_up:
+    :return str:
+
+    >>> os.path.isdir(update_path('bci'))
+    True
+    """
+    path = os.path.expanduser(path)
+    for _ in range(max_up):
+        if os.path.exists(path):
+            break
+        path = os.path.join('..', path)
+    return path
 
 
 def load_data(file_name, float_mean=5):
@@ -17,6 +37,11 @@ def load_data(file_name, float_mean=5):
     :param float float_mean: floating mean in seconds
     :return:
 
+    >>> p_data = os.path.join(update_path('local_data'), 'blinking', 'both.bin')
+    >>> eeg = load_data(p_data)  # doctest: +ELLIPSIS
+    ...
+    >>> len(eeg) > 0
+    True
     """
     ft_mean_samples = int(float_mean * SAMPLE_FREQ)
     data = np.fromfile(file_name, dtype='float32')
@@ -24,14 +49,19 @@ def load_data(file_name, float_mean=5):
     eeg = data[:, :8]
     means = np.array([np.mean(eeg[i:ft_mean_samples+i], axis=0)
                       for i in range(len(eeg) - ft_mean_samples)])
-    print (means.shape)
     eeg = eeg[ft_mean_samples:] - means
     for i in range(eeg.shape[1]):
-        eeg[:, i] = filter.notch_filter(x=eeg[:, i].astype(np.float64),
-                                        Fs=SAMPLE_FREQ,
-                                        freqs=[50],
-                                        trans_bandwidth=2)
-        b, a = signal.butter(5, Wn=np.array([0.5, 30]) / (0.5 * SAMPLE_FREQ),
-                             btype='band')
-        eeg[:, i] = signal.lfilter(b, a, eeg[:, i])
+        eeg[:, i] = filter_signal(eeg[:, i])
     return eeg
+
+
+def filter_signal(sig):
+    sig = filter.notch_filter(x=sig.astype(np.float64),
+                              Fs=SAMPLE_FREQ,
+                              freqs=[50],
+                              trans_bandwidth=2,
+                              verbose=0)
+    b, a = signal.butter(5, Wn=np.array([0.5, 30]) / (0.5 * SAMPLE_FREQ),
+                         btype='band')
+    sig = signal.lfilter(b, a, sig)
+    return sig
