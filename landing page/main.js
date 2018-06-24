1e2e265271d03c086608aed1dccc97d31b752685
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2018-06-24 11:44:14 +0200

    add websocket stuff

--- landing page/main.js
+++ landing page/main.js
@@ -1,4 +1,6 @@
 (function (jquery) {
+
+  // ROW MACHINE
   jquery(".row").machine({
     inactive: {
       defaultState: true,
@@ -17,5 +19,53 @@
         click: "inactive"
       }
     }
+  })
+
+
+  // WEBSOCKETS STUFF
+  var webSocket = jquery.simpleWebSocket({
+    url: 'ws://localhost:8000/ws/bci/U123/'
+  })
+
+    // reconnected listening
+    webSocket.listen(function(message) {
+      var data = message.data
+      console.log(data)
+      if (data.amount === 1) {
+        // single blinking
+        if (data.eyes === 'right') {
+          console.log('trigger right arrow')
+        } else if (data.eyes === 'left') {
+          console.log('trigger left arrow')
+        } else {
+          // both eyes
+          console.log('trigger single click')
+        }
+      } else {
+        // double blinking
+        console.log('trigger double click')
+      }
+    })
+
+  // MAIN LOOP
+  var focusedRowIndex = 0
+  var activeRow = jquery('.row').first()
+
+  var focusedLetterBox = null
+  var focusedLetter = null
+
+  var activeLetterBox = null
+  var activeLetter = null
+
+  function mainLoop () {
+    var activeRow = jquery('.row-selected')
+    if (activeRow.length == 1) {
+      activeRow = activeRow.first()
+    } else {
+    }
+  }
+
+  jquery(document).ready(function () {
+   setInterval(mainLoop, 1000);
   })
 })($ || {})
